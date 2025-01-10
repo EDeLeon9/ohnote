0e8acfa3c5046b297550de031dcf4ea96c845075
Esteven De Leon <edeleon4533cib@gmail.com>
2025-01-10 15:39:00 -0500

    About page finished

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ohnote/data/app_theme.dart';
+import 'package:ohnote/tools/single_async.dart';
 import 'package:ohnote/view_components/header_buttons.dart';
 import 'package:ohnote/tools/custom_toast.dart' as t;
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
+
+  static const email = 'trendsappsdev@gmail.com';
+
+  void _copyToClipboard(BuildContext context) {
+    runFirst(() async {
+      await Clipboard.setData(ClipboardData(text: email));
+      if (context.mounted) {
+        t.showCustomToast('Email copied to clipboard', context);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,25 +30,31 @@ class AboutPage extends StatelessWidget {
             children: [
               Positioned.fill(
                 child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Image(
-                        height: 45.0,
-                        width: 45.0,
+                        height: 100.0,
+                        width: 100.0,
                         image: AssetImage('assets/icon/icon.png'),
                       ),
                       Text('Version: 1.0.0'),
+                      SizedBox(height: 20.0),
                       Text('Contact dev:'),
                       TextButton(
-                          onPressed: () {
-                            //TODO: copy to clipboard
-                            t.showCustomToast('Email copied to clipboard', context);
-                          },
-                          child: Text('trendsappsdev@gmail.com')),
+                        style: ButtonStyle(
+                          padding: WidgetStateProperty.all(EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0)),
+                          minimumSize: WidgetStateProperty.all(Size.zero),
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        ),
+                        onPressed: () => _copyToClipboard(context),
+                        onLongPress: () => _copyToClipboard(context),
+                        child: Text(email),
+                      ),
+                      SizedBox(height: 20.0),
                       Text(
-                        'Copyright © 2025 Trends Apps. OhNote is a trademark of Trends Apps. All rights reserved.',
+                        '© 2025 Trends Apps. OhNote is a trademark of Trends Apps. All rights reserved.',
                         textAlign: TextAlign.center,
                       ),
                     ],
